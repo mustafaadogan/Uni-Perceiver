435a37724992c04623cce61e52602f2edb7b9753
Mustafa <dogan_mustafa@hacettepe.edu.tr>
2023-08-06 12:15:24 +0300

    Update retrieval_evaler.py

--- uniperceiver/evaluation/retrieval_evaler.py
+++ uniperceiver/evaluation/retrieval_evaler.py
@@ -1,4 +1,3 @@
-
 import os
 import sys
 import numpy as np
@@ -11,45 +10,22 @@ import json
 class RetrievalEvaler(object):
     def __init__(self, cfg, annfile, output_dir,):
         super(RetrievalEvaler, self).__init__()
-        self.eval_bs = cfg.INFERENCE.EVAL_BS
-        self.anno_file = cfg.INFERENCE.TEST_ANNFILE
-        self.output_dir = output_dir
         pass
 
-    def eval(self, vfeats, tfeats, labels, prefix=None):
-        count = 0
+    def eval(self, vfeats, tfeats, index):
 
-        batch_size = self.eval_bs
-        batch_num = vfeats.size(0) // batch_size
-        batch_num_t = tfeats.size(0) // batch_num
+        similarity_scores_dict = {}
 
-        scores = []
-
-        for i in range(batch_num):
-            if i == batch_num - 1:
-                b_tfeats = tfeats[i*batch_num_t:]
-                b_vfeats = vfeats[i*batch_size:]
-            else:
-                b_tfeats = tfeats[i*batch_num_t:(i+1)*batch_num_t]
-                b_vfeats = vfeats[i*batch_size:(i+1)*batch_size]
+        for text_index in range(tfeats.size()[0]):
+            relevant_video_features = vfeats[0]
+            text_feature = tfeats[text_index]
 
             with torch.no_grad():
-                score = (b_tfeats.unsqueeze(1) * b_vfeats.unsqueeze(0)).sum(dim=-1).sum(dim=0).cpu().numpy()
-                scores.append(float(score[0]))
-        
-        annoinfo = json.load(open(self.anno_file))
-        main_task_results = {}
-        prof_results = {}
-        consumed_idx = 0
+                score = (text_feature * relevant_video_features).sum(dim=-1).cpu().numpy()
 
-        for key in annoinfo.keys():
-            main_task_results[key] = {"scores" : scores[consumed_idx : consumed_idx + 1 + len(annoinfo[key]["foils"])]}
-            consumed_idx = 1 + len(annoinfo[key]["foils"])
-            prof_results[key] = {"scores" : scores[consumed_idx : consumed_idx + 1 + len(annoinfo[key]["proficiency"]["foils"])]}
-            consumed_idx = 1 + len(annoinfo[key]["proficiency"]["foils"])
-        
-        with open(f"{self.output_dir}/Main_Task_Results.json", "w") as task_outfile:
-            json.dump(main_task_results, task_outfile, indent=4)
+            if index in similarity_scores_dict.keys():
+                    similarity_scores_dict[index] += float(score)
+            else:
+                similarity_scores_dict[index] = float(score)
 
-        with open(f"{self.output_dir}/Prof_Results.json", "w") as prof_outfile:
-            json.dump(prof_results, prof_outfile, indent=4)
+        return similarity_scores_dict[index]
